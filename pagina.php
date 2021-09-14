925ceb53314ddb2af81a27deb95f40674ff29bc2
Arthur-1702 <arthur.de.lima.araujo@gmail.com>
2021-09-14 20:11:43 -0300

    pagina_web

---
+++ pagina.php
@@ -0,0 +1,38 @@
+<html>
+
+
+	<head>
+		<title> Meu primeiro site em PHP! </title>
+		
+		<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+		
+		<style type="text/css">
+			.linha{
+				font-weight: bold;
+				color: blue;
+				paddinng-left: 10px;
+				line-height: 50px;
+			}
+		</style>
+
+	</head>
+	
+	<body>
+	
+		<?php
+			for($i = 0 ; $i < 10 ; $i++){
+				print("<span class=\"linha\">Linha numero ". $i . "</span><br />");
+			}
+		?>
+		
+	</body>
+	
+	<script type="text/javascript">
+		$(document).ready(function(){
+			alert("UEPA! Ratinho!");
+		});
+	</script>
+			
+	
+
+</html>
